efcf3d74aaab79b18ba0b0191118d45145f27d7f
Hugues Ody <huguesody@gmail.com>
2025-07-08 00:55:21 +0000

    update condo.php

--- coeur/modules/template/views/sections/condos.php
+++ coeur/modules/template/views/sections/condos.php
@@ -2,67 +2,88 @@
     <div class="container-fluid p-0">
         <div class="row no-gutters">
             <div class="col-lg-3 col-md-6">
-                <div class="condo-item" style="background-image: url(<?php echo base_url()?>assets/front-end/img/items/menu-gallery-1.jpg);">
+                <div class="condo-item" style="background-image: url(<?php echo base_url() ?>assets/front-end/img/items/condos1.jpeg);">
                     <div class="title">
                         <i class="fal fa-plus text-white"></i>
-                        <h4 class="text-white">Handmade Earring</h4>
+                        <h4 class="text-white">AKOFENA</h4>
                     </div>
                     <div class="title title-hidden">
-                        <h4 class="text-white mt-0">Handmade Earring</h4>
-                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
-                        <div class="book-btn">
-                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i>Buy now</a>
+                        <h4 class="text-white mt-0">AKOFENA</h4>
+                        <p class="text-white">
+                            Des pierres volcaniques pour symboliser la résilience,
+                            de l’hématite dorée pour l’élégance et la puissance,
+                            et de l’Agate rouge pour la passion et la détermination.
+                            AKOFENA, c’est le rappel que tu es digne, fort.e et prêt.e à affronter la vie.
+
+                        </p>
+                        <div class="book-btn  text-white">
+                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i>Achetez maintenant</a>
                         </div>
                     </div>
-                    <a href="shop-detail.html" class="main-btn btn-border">Starting at $299</a>
+                    <a href="shop-detail.html" class="main-btn btn-border text-white">à partie de 10.000f</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="condo-item" style="background-image: url(<?php echo base_url()?>assets/front-end/img/items/menu-gallery-2.jpg);">
+                <div class="condo-item" style="background-image: url(<?php echo base_url() ?>assets/front-end/img/items/condos2.jpeg);">
                     <div class="title">
                         <i class="fal fa-plus text-white"></i>
-                        <h4 class="text-white">Diamond Nosering</h4>
+                        <h4 class="text-white">Pierres d'Agate </h4>
                     </div>
                     <div class="title title-hidden">
-                        <h4 class="text-white mt-0">Diamond Nosering</h4>
-                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
-                        <div class="book-btn">
-                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i> Buy now </a>
+                        <h4 class="text-white mt-0">Pierres d'Agate </h4>
+                        <p class="text-white">
+                            Pierres d'Agate rouge rouge et orange, onyx noir brillant.
+                            Un bijou qui évoque la force et la passion, parfait pour les moments spéciaux.
+                        </p>
+                        <div class="book-btn text-white">
+                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i> Achetez maintenant</a>
                         </div>
                     </div>
-                    <a href="shop-detail.html" class="main-btn btn-border">Starting at $499</a>
+                    <a href="shop-detail.html" class="main-btn btn-border text-white">à parti de 10 000fr</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="condo-item" style="background-image: url(<?php echo base_url()?>assets/front-end/img/items/menu-gallery-3.jpg);">
+                <div class="condo-item" style="background-image: url(<?php echo base_url() ?>assets/front-end/img/items/condo3.jpg);">
                     <div class="title">
                         <i class="fal fa-plus text-white"></i>
-                        <h4 class="text-white">Silver Bracelet</h4>
+                        <h4 class="text-white">Wawa Aba</h4>
                     </div>
                     <div class="title title-hidden">
-                        <h4 class="text-white mt-0">Silver Bracelet</h4>
-                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
-                        <div class="book-btn">
-                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i> Book now</a>
+                        <h4 class="text-white mt-0">Wawa Aba</h4>
+                        <p class="text-white">
+                            Le symbole "Wawa Aba" est un motif traditionnel issu de la culture Akan,
+
+                            Il symbolise en effet Résistance, endurance et persévérance
+                        </p>
+                        <div class="book-btn text-white">
+                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i> Achetez maintenant</a>
                         </div>
                     </div>
-                    <a href="shop-detail.html" class="main-btn btn-border">Starting at $299</a>
+                    <a href="shop-detail.html" class="main-btn btn-border text-white">à parti de 10 000 fr</a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="condo-item" style="background-image: url(<?php echo base_url()?>assets/front-end/img/items/menu-gallery-4.jpg);">
+                <div class="condo-item" style="background-image: url(<?php echo base_url() ?>assets/front-end/img/items/condo4.jpg);">
                     <div class="title">
                         <i class="fal fa-plus text-white"></i>
-                        <h4 class="text-white">Gold Pendents</h4>
+                        <h4 class="text-white">Elegance</h4>
                     </div>
                     <div class="title title-hidden">
-                        <h4 class="text-white mt-0"> Gold Pendents</h4>
-                        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
-                        <div class="book-btn">
-                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i>Book now </a>
+                        <h4 class="text-white mt-0"> Elegance</h4>
+                        <p class="text-white">
+                            Vous vous offrons le meilleur, qualité et le prix.
+                            Des pierres volcaniques pour symboliser la résilience,
+                            de l’hématite dorée pour l’élégance et la puissance.
+                        </p>
+                        <div class="book-btn text-white">
+                            <a href="shop-detail.html"><i class="fal fa-long-arrow-right"></i>
+                                Achetez maintenant
+                            </a>
                         </div>
                     </div>
-                    <a href="shop-detail.html" class="main-btn btn-border">Starting at $199</a>
+                    <a href="shop-detail.html" class="main-btn btn-border text-white">
+                        à parti de 13 000 fr
+                    </a>
                 </div>
             </div>
         </div>
